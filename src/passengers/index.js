accbf834d2c4708c15725d59f7086b2dfc2e316c
idoyun655-a11y <idoyun655@gmail.com>
2026-09-16 23:08:51 +0900

    feat: export passenger simulation modules

---
+++ src/passengers/index.js
@@ -0,0 +1,5 @@
+export { Passenger, PASSENGER_TYPES, PASSENGER_STATUSES } from './model.js';
+export { QueueEngine, QUEUE_DISCIPLINES } from './queue.js';
+export { PassengerSimulationSystem } from './system.js';
+export { CheckInZone, SecurityZone, ImmigrationZone, IMMIGRATION_STATUSES } from './services.js';
+export { PASSENGER_ARRIVAL_MODEL, PASSENGER_SERVICE_CONFIG, PASSENGER_KPI_SOURCE_POLICY } from './config.js';
