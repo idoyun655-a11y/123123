7bf4eb1f65e972f9bbe56acbe9712afb412e0133
idoyun655-a11y <idoyun655@gmail.com>
2026-09-16 20:50:01 +0900

    feat: export airport data models

---
+++ src/data/index.js
@@ -0,0 +1,2 @@
+export { SOURCE_TYPES, OPERATING_STATUSES, MAINTENANCE_STATUSES, AirportObject, Models } from './model.js';
+export { INCHEON_AIRPORT_DATA, flattenIncheonDataset } from './incheon.js';
