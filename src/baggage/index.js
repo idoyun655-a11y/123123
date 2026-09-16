2eca0b233aec37414d69ac5bf6fce587f809adc1
idoyun655-a11y <idoyun655@gmail.com>
2026-09-16 23:54:36 +0900

    export screening resource

--- src/baggage/index.js
+++ src/baggage/index.js
@@ -1,4 +1,4 @@
-export { Baggage, BAGGAGE_TYPES, BAGGAGE_STATUSES } from './model.js';
+export { Baggage, BaggageScreeningResource, BAGGAGE_TYPES, BAGGAGE_STATUSES } from './model.js';
 export { BAGGAGE_CONFIG, BAGGAGE_DATA_POLICY, BHS_SERVICE_TYPES } from './config.js';
 export { BaggageHandlingSystem } from './system.js';
 export { GroundTask, GroundResource, GROUND_TASK_STATUS, GROUND_TASK_TYPES, GROUND_RESOURCE_TYPES, GROUND_RESOURCE_STATUS } from '../ground/model.js';
