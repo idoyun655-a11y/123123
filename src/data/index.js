b2868ebb1ded5577286f8b20a9a1b2e9fb7228e1
idoyun655-a11y <idoyun655@gmail.com>
2026-09-16 20:51:37 +0900

    feat: export airport spatial map data

--- src/data/index.js
+++ src/data/index.js
@@ -1,2 +1,3 @@
 export { SOURCE_TYPES, OPERATING_STATUSES, MAINTENANCE_STATUSES, AirportObject, Models } from './model.js';
 export { INCHEON_AIRPORT_DATA, flattenIncheonDataset } from './incheon.js';
+export { MAP_UNITS, INCHEON_SPATIAL_MAP, SPATIAL_SOURCE, getSpatialObjects } from './spatial.js';
