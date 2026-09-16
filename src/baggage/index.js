e80e0f5fa082f5cee4186319296be6079bfefb04
idoyun655-a11y <idoyun655@gmail.com>
2026-09-17 00:11:48 +0900

    fix(stage8-regression): expose QueueEngine-compatible baggage queue lookup

--- src/baggage/index.js
+++ src/baggage/index.js
@@ -1,5 +1,7 @@
 export { Baggage, BaggageScreeningResource, BAGGAGE_TYPES, BAGGAGE_STATUSES } from './model.js';
 export { BAGGAGE_CONFIG, BAGGAGE_DATA_POLICY, BHS_SERVICE_TYPES } from './config.js';
-export { BaggageHandlingSystem } from './system.js';
+import { BaggageHandlingSystem } from './system.js';
+if (typeof BaggageHandlingSystem.prototype.getQueue !== 'function') Object.defineProperty(BaggageHandlingSystem.prototype, 'getQueue', { value(queueId) { return this.queueEngine.getQueue(queueId); } });
+export { BaggageHandlingSystem };
 export { GroundTask, GroundResource, GROUND_TASK_STATUS, GROUND_TASK_TYPES, GROUND_RESOURCE_TYPES, GROUND_RESOURCE_STATUS } from '../ground/model.js';
 export { GroundHandlingSystem } from '../ground/system.js';
