9b61c6887798e0b3a9fd66d132282fcfc829a14e
idoyun655-a11y <idoyun655@gmail.com>
2026-09-16 20:53:14 +0900

    fix: correct flight lifecycle test timing

--- test/flight.test.js
+++ test/flight.test.js
@@ -39,9 +39,9 @@ test('arrival lifecycle follows simulation time', () => {
   assert.equal(flight.status, FLIGHT_STATUSES.LANDED);
   simulation.tick(10 * 60_000);
   assert.equal(flight.status, FLIGHT_STATUSES.AT_GATE);
-  simulation.tick(30 * 60_000);
+  simulation.tick(5 * 60_000);
   assert.equal(flight.status, FLIGHT_STATUSES.BOARDING);
-  simulation.tick(30 * 60_000);
+  simulation.tick(20 * 60_000);
   assert.equal(flight.status, FLIGHT_STATUSES.READY);
 });
 
