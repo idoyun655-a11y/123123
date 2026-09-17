79f19c13b1cb04646dcbe5f320cd473378329c15
idoyun655-a11y <idoyun655@gmail.com>
2026-09-17 16:51:56 +0900

    feat(stage14): export incident system

---
+++ src/incidents/index.js
@@ -0,0 +1,3 @@
+export { IncidentSystem } from './system.js';
+export { Incident, ResponseTask } from './model.js';
+export { INCIDENT_TYPES, INCIDENT_STATUS, INCIDENT_SEVERITIES, RESPONSE_ROLES, INCIDENT_DATA_POLICY } from './config.js';
